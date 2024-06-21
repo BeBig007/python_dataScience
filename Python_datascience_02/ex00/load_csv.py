959cb43d12b67828fcef63ed327bcb117a2b7b02
user <bigel.beatrice@gmail.com>
2024-06-21 17:26:25 +0200

    prepare DS02

---
+++ Python_datascience_02/ex00/load_csv.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def load(path: str) -> Dataset: # (You have to adapt the type of return according to your library)
+    return None
+
+# $> python3 tester.py
+# Loading dataset of dimensions (195, 302)
+# country 1800 1801 1802 1803 ... 2096 2097 2098 2099 2100
+# Afghanistan 28.2 28.2 28.2 28.2 ... 76.2 76.4 76.5 76.6 76.8
+# ...
+# $>
